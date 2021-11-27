38e64071cd64ccc7831ee364dbd75015d8c79027
María Moreno <mgm9211@gmail.com>
2021-11-27 19:03:48 +0100

    login view and template

--- web/views.py
+++ web/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# Login
+from django.contrib.auth import login as do_login
+from django.contrib.auth import authenticate
 
 # Create your views here.
+
+def login(request):
+    context = {}
+
+    if request and request.POST:
+        print('-request.POST', request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            do_login(request, user)
+            return redirect('index')
+
+    return render(request, 'login.html', context)
+
 
 def index(request):
     context = {}
