94c21facbcf0bd4901524df47df5671e0ce2dd31
María Moreno <mgm9211@gmail.com>
2021-11-27 19:04:35 +0100

    deleted AuthUser, created ProfileUser model and updated requirements with Pillow

--- web/models.py
+++ web/models.py
@@ -1,18 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
 
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
+class ProfileUser(models.Model):
+    phone_number = models.CharField(max_length=16)
+    image_profile = models.ImageField(upload_to='users/image_profiles', default=True, blank=True, null=True)
+    visible = models.IntegerField()
+    upload_at = models.DateTimeField(blank=True, null=True)
     username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
     is_staff = models.IntegerField()
     is_active = models.IntegerField()
+
+    def get_user(self):
+        return User.objects.get(username=self.username)
 
 
 class Product(models.Model):
