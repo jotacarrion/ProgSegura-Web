7271fe3b8bc14ec089bb0428e516bf61ef998870
María Moreno <mgm9211@gmail.com>
2021-11-28 19:59:14 +0100

    updated templates, created form to update product, showing products in index...

--- web/models.py
+++ web/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class ProfileUser(models.Model):
     phone_number = models.CharField(max_length=16)
-    #image_profile = models.ImageField(upload_to='users/image_profiles', default=True, blank=True, null=True)
+    image_profile = models.ImageField(upload_to='users/image_profiles', default=True, blank=True, null=True)
     username = models.OneToOneField(User, models.DO_NOTHING, unique=True)
 #    is_active = models.IntegerField()
 
@@ -16,4 +16,5 @@ class ProfileUser(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=2048)
+    image_product = models.ImageField(upload_to='products', default=True, blank=True, null=True)
     price = models.FloatField()
--- web/views.py
+++ web/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login as do_login
 from django.contrib.auth import authenticate
 
 from django.contrib.auth.models import User
-from web.models import ProfileUser
+from web.models import Product, ProfileUser
 
 # Create your views here.
 
@@ -27,36 +27,77 @@ def login(request):
 def index(request):
     context = {}
 
+    all_products = Product.objects.all()
+    context['all_products'] = all_products
+
+
+
     return render(request, 'index.html', context=context)
 
 
 #@login_required
-#def user_detail(request, user_id):
-def user_detail(request):
+def user_detail(request, user_id):
     context = {}
-    user_id = 1
     if User.objects.filter(id=user_id).exists():
-
         user = User.objects.get(id=user_id)
+        context['user'] = user
 
         username = user.username
-        context['user_detail'] = None
         user_detail = None
+
         if ProfileUser.objects.filter(username=user).exists():
             user_detail = ProfileUser.objects.get(username=user)
-            context['user_detail'] = user_detail
+
+        if request.FILES:
+            if 'image_profile' in request.FILES and request.FILES['image_profile']:
+                if ProfileUser.objects.filter(username=user).exists():
+                    profile_user_viewing = ProfileUser.objects.get(username=user)
+                else:
+                    profile_user_viewing = ProfileUser.objects.create(username=user, phone_number=None, image_profile=None)
+                profile_user_viewing.image_profile = request.FILES['image_profile']
+                profile_user_viewing.save()
+
 
         if request.POST:
-            first_name = request.POST['first_name']
-            last_name = request.POST['last_name']
+            if 'first_name' in request.POST and 'last_name' in request.POST:
+                first_name = request.POST['first_name']
+                last_name = request.POST['last_name']
 
-            User.objects.filter(username=username).update(first_name=first_name, last_name=last_name)
-            if 'phone' in request.POST and request.POST['phone']:
-                phone = request.POST['phone']
-                if not user_detail and phone:
-                    ProfileUser.objects.create(username=user, phone_number=phone)
-                else:
-                    ProfileUser.objects.filter(username=user).update(phone_number=phone)
-                return redirect('user-detail')
+                User.objects.filter(username=username).update(first_name=first_name, last_name=last_name)
+                if 'phone' in request.POST and request.POST['phone']:
+                    phone = request.POST['phone']
+                    if not user_detail and phone:
+                        ProfileUser.objects.create(username=user, phone_number=phone)
+                    else:
+                        ProfileUser.objects.filter(username=user).update(phone_number=phone)
+                    return redirect('user-detail')
+
+        context['user_detail'] = user_detail
 
     return render(request, 'user-detail.html', context=context)
+
+
+
+#@login_required
+def product_update(request, product_id):
+    context = {}
+
+    if Product.objects.filter(id=product_id).exists():
+        product = Product.objects.get(id=product_id)
+        context['product'] = product
+
+        if request.FILES:
+            if 'image_product' in request.FILES and request.FILES['image_product']:
+                product.image_product = request.FILES['image_product']
+                product.save()
+
+        if request.POST:
+            if 'name' in request.POST and 'name' in request.POST:
+                name = request.POST['name']
+                description = request.POST['description']
+                price = request.POST['price']
+
+                Product.objects.filter(id=product_id).update(name=name, description=description, price=price)
+            return redirect('product-update', product_id)
+
+    return render(request, 'product-update.html', context=context)
